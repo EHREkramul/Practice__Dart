a1786c8c33093d6b543d356b7f93fa5a14a404ef
EHREkramul <ehr.ekramul@gmail.com>
2024-12-20 18:56:44 +0600

    Enhance encapsulation and inheritance

--- bin/Module-3/encapsulation.dart
+++ bin/Module-3/encapsulation.dart
@@ -1,5 +1,3 @@
-import 'restaurant.dart';
-
 void main(){
   Restaurant bfc = Restaurant();
   // bfc._zoneName; // Error. Private field
@@ -10,3 +8,32 @@ void main(){
   print(bfc.order('Chicken Bucket'));
 }
 
+class Restaurant{
+  Restaurant(){
+    _shopping();
+  }
+
+  final _restaurantName = 'BFC';
+  // const String _name = "BFC"; // Only static field can be const. static const String _name = "BFC";
+  // late const String _name1 = "BFC"; // Error. Members can't be declared to be both 'const' and 'late'.
+  late String _customerName;
+  String get getCustomerName => _customerName; // Getter Method. Call: bfc.getCustomerName
+  // String set setCustomerName (name) => _customerName=name; // Setter Method. Call: bfc.setCustomerName = 'Ekramul Haque';. The return type of the setter must be 'void' or absent/nothing.
+  set setCustomerName (name) => _customerName=name; // Setter Method. Call: bfc.setCustomerName = 'Ekramul Haque';
+  /*set setCustomerName(String name){ // Another alternative to define setter method // Call(same): bfc.setCustomerName = 'Ekramul Haque';
+    _customerName = name;
+  }*/
+
+  String order (String item){
+    _prepareItem(item);
+    return '$item served';
+  }
+
+  _prepareItem(String item){
+    print('Preparing $item');
+  }
+  _shopping(){
+    print('Shopping for $_restaurantName Opening');
+  }
+
+}
--- bin/Module-3/inheritance.dart
+++ bin/Module-3/inheritance.dart
@@ -1,19 +1,66 @@
 class Father{
+  Father(){
+    print("Father constructor");
+  }
   totalTaka(){
     print("Father's Money = 80,000 ");
   }
 }
 
 class Son extends Father{
-  totalTaka(){
-    print("Son's Money = 50,000 "); // Method Overriding
+  // Son(); // Empty Constructor
+  Son(){
+    print('Son Constructor');
+  }
+  @override // this annotation is Optional. Using for convention only to identify the override method.
+  totalTaka(){ // Method Overriding
+    print("Son's Money = 50,000 ");
   }
 }
 
+class Vehicle{
+  int speed;
+  int cc;
+  String model;
+
+  Vehicle(this.speed, this.cc, this.model);
+  void start(){
+    print("Vehicle Starts");
+  }
+  void stop(){
+    print('Vehicle Stops');
+  }
+  void getcc(){
+    print('Bike cc is: $cc');
+  }
+}
+
+class Bike extends Vehicle{
+  String bikeName;
+
+  Bike(speed, cc, model, this.bikeName):super(speed, cc, model); // Passing values to the parent class constructor
+  // Bike(super.speed, super.model, this.bikeName, super.cc); // Alternative way to assign parent constructor parameters. parameter order doesn't matter.
+
+  @override
+  void start(){
+    super.start();
+    print('$bikeName Starts');
+  }
+  @override
+  void stop(){
+    super.stop();
+    print('$bikeName Stops');
+  }
+
+}
 void main(){
   var f1 = Father();
   f1.totalTaka();
 
   var s1 = Son();
   s1.totalTaka();
+
+  Bike bike_1 = Bike(120, 250, 'v3-2024', 'Yamaha');
+  bike_1.start();
+  bike_1.getcc();
 }
