f1f6dcfdf64723fe1a14a8473d148d38ce0e5a49
EHREkramul <ehr.ekramul@gmail.com>
2024-12-20 19:10:01 +0600

    Enhance abstract

--- bin/Module-3/abstract.dart
+++ bin/Module-3/abstract.dart
@@ -1,5 +1,11 @@
 abstract class Father{
-  totalTaka(){}
+  // totalTaka(); // Valid. Must be implemented body into sub classes. Non-Abstract class can not have method without body
+  totalTaka(){ // Implementation in child class is optional. by default this body will be used.
+    print('Father\'s Money = 80,000');
+  }
+  static identify(){
+    print('I am Father');
+  }
 }
 
 class Son extends Father{
@@ -10,6 +16,7 @@ class Son extends Father{
 
 void main(){
   // var f1 = Father(); // Object of abstract class can't be created
+  Father.identify(); // Static member can be accessible
 
   var s1 = Son();
   s1.totalTaka();
